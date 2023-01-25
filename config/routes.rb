29335546341e758a3a4359d9b549d9ca7e751aa6
huwtyler <huwjtyler@googlemail.com>
2023-01-25 11:54:07 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
+
+# get /users/sign_in - sign in from
+# get /users/sign_up - sign up form
