8045c800b85751adc9fd1e32842fde05109a8414
huwtyler <huwjtyler@googlemail.com>
2023-01-28 20:09:28 +0000

    Add search bar and style header

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
       },
     },
   },
